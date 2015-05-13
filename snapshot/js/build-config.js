83fbdb90dc1b8823156fbd2a1258022a93ddb723
Matthew Findley <findley@substantial.com>
2015-05-13 14:38:27 -0700

    snapshot: 1234

---
+++ snapshot/js/build-config.js
@@ -0,0 +1,7 @@
+DocsApp.constant('BUILDCONFIG', {
+  "ngVersion": "1.3.15",
+  "version": "0.9.0",
+  "repository": "https://github.com/angular/material",
+  "commit": "e4ebd59ce38c4dec5cb7777f0b618707ef793a1d",
+  "date": "2015-05-12 20:27:59 -0500"
+});
